29bb2c6d96894bc23b609320053eb9a05c6dd6e8
SmartDevCI <31289611+smartdevci@users.noreply.github.com>
2017-10-30 13:28:54 +0000

    footer2

--- dashboard/Inc/footer.php
+++ dashboard/Inc/footer.php
@@ -1,6 +1,6 @@
 <footer class="text-center navbar navbar-fixed-bottom" style="color: #fff; background-color: rgb(16,99,168);">
     <p style="font-size: 18px; padding: 10px 0;"><?php echo date('Y'); ?> Copyright &copy;
-        <a href="https://www.appsmartdev.com/" target="_blank" style="text-decoration: none; color: #a6cad8">SmartDev</a>
+        <a href="https://www.smartdevci.com/" target="_blank" style="text-decoration: none; color: #a6cad8">SmartDev</a>
         - Tous droits reservés
     </p>
 </footer>
